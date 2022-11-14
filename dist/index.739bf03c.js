61ccc5c1816369e896214b944555b7f0982919a1
hsiao.yihan <hsiao.yihan@linecorp.com>
2022-11-14 19:01:58 +0900

    fix the button bug

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -6380,6 +6380,6 @@ var SplitType = /*#__PURE__*/ function() {
 }();
 exports.default = SplitType;
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["cVgJb","ebWYT"], "ebWYT", "parcelRequire2a82")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["cVgJb","ebWYT"], "ebWYT", "parcelRequirecbdb")
 
 //# sourceMappingURL=index.739bf03c.js.map
